2d1160e23503de5c2c37807f517b9f9c94e2c625
Théo Capitaine <totoche22300@gmail.com>
2022-10-25 16:53:11 +0200

    ux design - multithread flash

--- OurAndroidApp/app/src/main/java/com/example/ourandroidapp/MainActivity.java
+++ OurAndroidApp/app/src/main/java/com/example/ourandroidapp/MainActivity.java
@@ -10,31 +10,37 @@ import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CameraManager mCameraManager;
-    private String mCameraId;
+    private volatile boolean stopThread = false;
+    public static CameraManager mCameraManager;
+    public static String mCameraId;
     private TextView textToConvert;
     private TextView morseResult;
+    private String convertedTxt;
     private Button toMorseBtn;
     private Button clearBtn;
-    private Button morseToLightBtn;
-    private boolean statusLight = false;
+    private Button morseToLSBtn;
+    private Button stopLSBtn;
+    private Switch switchLight;
+    private Switch switchSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textToConvert = (TextView) findViewById(R.id.textToConvert);
-        morseResult = (TextView) findViewById(R.id.morseResult);
-        toMorseBtn = (Button) findViewById(R.id.toMorseBtn);
-        clearBtn = (Button) findViewById(R.id.clearBtn);
-        morseToLightBtn = (Button) findViewById(R.id.morseToLightBtn);
+        textToConvert = findViewById(R.id.textToConvert);
+        morseResult = findViewById(R.id.morseResult);
+        toMorseBtn = findViewById(R.id.toMorseBtn);
+        clearBtn = findViewById(R.id.clearBtn);
+        morseToLSBtn = findViewById(R.id.morseToLSBtn);
+        stopLSBtn = findViewById(R.id.stopLSBtn);
+        switchLight = findViewById(R.id.switchLight);
+        switchSound = findViewById(R.id.switchSound);
 
         boolean isFlashAvailable = getApplicationContext().getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
@@ -54,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String txtToConvert = textToConvert.getText().toString();
-                String convertedTxt = MorseCode.alphaToMorse(txtToConvert);
+                convertedTxt = MorseCode.alphaToMorse(txtToConvert);
                 morseResult.setText(convertedTxt);
             }
         });
@@ -67,12 +73,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        morseToLightBtn.setOnClickListener(new View.OnClickListener() {
+        morseToLSBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switchFlashLight(statusLight);
-                statusLight = !statusLight;
+
+                if(switchLight.isChecked()){
+                    startThread(convertedTxt);
+                }
+                if(switchSound.isChecked()){
+                    // TODO
+                }
+            }
+        });
+
+        stopLSBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopThread();
             }
         });
     }
@@ -89,11 +106,58 @@ public class MainActivity extends AppCompatActivity {
         alert.show();
     }
 
-    public void switchFlashLight(boolean statusLight) {
+    public void switchFlashLight(boolean status) {
         try {
-            mCameraManager.setTorchMode(mCameraId, statusLight);
+            mCameraManager.setTorchMode(mCameraId, status);
         } catch (CameraAccessException e) {
             e.printStackTrace();
+        }
+    }
+
+
+
+    //Multithread
+
+    public void timeDelay(long t) {
+        try {
+            Thread.sleep(t);
+        } catch (InterruptedException e) {}
+    }
+
+    public void startThread(String msg){
+        stopThread = false;
+        MainRunnable runnable = new MainRunnable(msg);
+        new Thread(runnable).start();
+    }
+
+    public void stopThread(){
+        stopThread = true;
+    }
+
+    class MainRunnable implements Runnable{
+
+        String morseMsg;
+
+        MainRunnable(String morseMsg){
+            this.morseMsg = morseMsg;
+        }
+
+        @Override
+        public void run(){
+            for (int i=0; i < morseMsg.length(); i++) {
+                if(stopThread){return;}
+                if(morseMsg.charAt(i) == '.') {
+                    switchFlashLight(true);
+                    timeDelay(100);
+                }
+                else if(morseMsg.charAt(i) == '-') {
+                    switchFlashLight(true);
+                    timeDelay(500);
+                }
+                switchFlashLight(false);
+                timeDelay(300);
+                //Log.d("tag", String.valueOf(test));
+            }
         }
     }
 }
